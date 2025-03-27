99b2f2923eaaa84ae657b54f3d568e6553136edd
Ksenia Panova <rybinak95@gmail.com>
2025-03-27 23:08:56 +0300

    Add update and create schemas for directions

--- src/schemas.py
+++ src/schemas.py
@@ -16,14 +16,21 @@ class DisciplineRead(DisciplineCreate):
     id: Annotated[int, Field(example=1)]
 
 
-class DirectionBase(BaseModel):
+class DirectionCreate(BaseModel):
     name: Annotated[str, Field(example='Программная инженерия', max_length=50)]
     educational_level_id: Annotated[int, Field(example=1)]
     educational_form_id: Annotated[int, Field(example=1)]
     semester_count: Annotated[int, Field(example=8)]
 
 
-class DirectionRead(DirectionBase):
+class DirectionUpdate(BaseModel):
+    name: Annotated[str  | None, Field(example='Программная инженерия', max_length=50)]
+    educational_level_id: Annotated[int | None, Field(example=1)]
+    educational_form_id: Annotated[int | None, Field(example=1)]
+    semester_count: Annotated[int | None, Field(example=8)]
+
+
+class DirectionRead(DirectionCreate):
     id: Annotated[int, Field(example=1)]
 
 
