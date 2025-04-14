9fd31d3948d28098fc58e6368c9fbf4b9bdbd16f
Ksenia Panova <rybinak95@gmail.com>
2025-04-14 11:42:52 +0300

    Add http exceptions for competency groups

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,27 +1,35 @@
 from fastapi import HTTPException, status
 
 
-class DisciplineNotFoundException(HTTPException):
+class ActivityTypeNotFoundException(HTTPException):
     def __init__(self):
         super().__init__(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail='Дисциплина с указанным id не найдена.'
+            detail='Вид учебных занятий с указанным id не найден.'
         )
 
 
-class DisciplineNameIsNotUniqueException(HTTPException):
+class ActivityTypeNameIsNotUniqueException(HTTPException):
     def __init__(self):
         super().__init__(
             status_code=status.HTTP_409_CONFLICT,
-            detail='Дисциплина с таким наименованием уже существует.'
+            detail='Данный вид учебных занятий уже существует.'
         )
 
 
-class DisciplineShortNameIsNotUniqueException(HTTPException):
+class CompetencyGroupNotFoundException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Группа компетенций с указанным id не найдена.'
+        )
+
+
+class CompetencyGroupNameIsNotUniqueException(HTTPException):
     def __init__(self):
         super().__init__(
             status_code=status.HTTP_409_CONFLICT,
-            detail='Дисциплина с таким кратким наименованием уже существует.'
+            detail='Данная группа компетенций уже существует.'
         )
 
 
@@ -57,27 +65,19 @@ class DepartmentNameIsNotUniqueException(HTTPException):
         )
 
 
-class ActivityTypeNotFoundException(HTTPException):
-    def __init__(self):
-        super().__init__(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail='Вид учебных занятий с указанным id не найден.'
-        )
-
-
-class ActivityTypeNameIsNotUniqueException(HTTPException):
-    def __init__(self):
-        super().__init__(
-            status_code=status.HTTP_409_CONFLICT,
-            detail='Данный вид учебных занятий уже существует.'
-        )
-
-
 class DirectionNotFoundException(HTTPException):
     def __init__(self):
         super().__init__(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail='Direction not found.'
+            detail='Направление подготовки с указанным id не найдено.'
+        )
+
+
+class DirectionNameIsNotUniqueException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail='Данное направление подготовки уже существует.'
         )
 
 
@@ -85,5 +85,29 @@ class DisciplineBlockNotFoundException(HTTPException):
     def __init__(self):
         super().__init__(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail='Discipline block not found.'
+            detail='Блок дисциплины с указанным id не найден.'
+        )
+
+
+class DisciplineNotFoundException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Дисциплина с указанным id не найдена.'
+        )
+
+
+class DisciplineNameIsNotUniqueException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail='Дисциплина с таким наименованием уже существует.'
+        )
+
+
+class DisciplineShortNameIsNotUniqueException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail='Дисциплина с таким кратким наименованием уже существует.'
         )
