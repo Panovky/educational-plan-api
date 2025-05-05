b2c2d80185d2906160b29f14e298a96c39fc7048
Ksenia Panova <rybinak95@gmail.com>
2025-05-05 12:01:21 +0300

    Add field description in table competencies

--- src/models.py
+++ src/models.py
@@ -87,7 +87,8 @@ class Competency(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     code: Mapped[str] = mapped_column(String(10), nullable=False, unique=True)
-    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    description: Mapped[str] = mapped_column(String(255), nullable=False)
     competency_group_id: Mapped[int] = mapped_column(Integer, ForeignKey('competency_groups.id'))
 
     competency_group = relationship('CompetencyGroup', back_populates='competencies')
--- src/schemas.py
+++ src/schemas.py
@@ -39,17 +39,20 @@ class ActivityTypeRead(ActivityTypeCreate):
 
 
 class CompetencyCreate(BaseModel):
-    code: Annotated[str, Field(example='УК-1', max_length=10)]
-    name: Annotated[str, Field(example='Способен осуществлять поиск, критический анализ и синтез информации, '
-                                       'применять системный подход для решения поставленных задач.', max_length=255)]
+    code: Annotated[str, Field(example='УК-3', max_length=10)]
+    name: Annotated[str, Field(example='Командная работа и лидерство', max_length=30)]
+    description: Annotated[str, Field(
+        example='Способен осуществлять социальное взаимодействие и реализовывать свою роль в команде.', max_length=255
+    )]
     competency_group_id: Annotated[int, Field(gt=0, example=1)]
 
 
 class CompetencyUpdate(BaseModel):
-    code: Annotated[str | None, Field(example='УК-1', max_length=10)]
-    name: Annotated[str | None, Field(example='Способен осуществлять поиск, критический анализ и синтез информации, '
-                                              'применять системный подход для решения поставленных задач.',
-                                      max_length=255)]
+    code: Annotated[str | None, Field(example='УК-3', max_length=10)]
+    name: Annotated[str | None, Field(example='Командная работа и лидерство', max_length=30)]
+    description: Annotated[str | None, Field(
+        example='Способен осуществлять социальное взаимодействие и реализовывать свою роль в команде.', max_length=255
+    )]
     competency_group_id: Annotated[int | None, Field(gt=0, example=1)]
 
 
