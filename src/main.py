f3721a37b70f0f4592dab19b24b81f227bb8b0eb
Ksenia Panova <rybinak95@gmail.com>
2025-05-15 12:07:35 +0300

    Add directories for resources

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.routes import (
-    educational_levels, educational_forms, directions, activity_types, control_types, departments, disciplines,
-    competency_groups, competencies, indicators, map_cors, discipline_blocks, discipline_block_competencies,
-    direction_map_cors, validations
-)
-
+from src.educational_levels.routes import router as educational_levels_router
+from src.educational_forms.routes import router as educational_forms_router
+from src.directions.routes import router as directions_router
+from src.map_cors.routes import router as map_cors_router
+from src.direction_map_cors.routes import router as direction_map_cors_router
+from src.departments.routes import router as departments_router
+from src.disciplines.routes import router as disciplines_router
+from src.activity_types.routes import router as activity_types_router
+from src.control_types.routes import router as control_types_router
+from src.competency_groups.routes import router as competency_groups_router
+from src.competencies.routes import router as competencies_router
+from src.indicators.routes import router as indicators_router
+from src.discipline_blocks.routes import router as discipline_blocks_router
+from src.discipline_block_competencies.routes import router as discipline_block_competencies_router
+from src.validations.routes import router as validations_router
 
 app = FastAPI()
 
@@ -17,18 +26,18 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
-app.include_router(educational_levels.router)
-app.include_router(educational_forms.router)
-app.include_router(directions.router)
-app.include_router(activity_types.router)
-app.include_router(control_types.router)
-app.include_router(departments.router)
-app.include_router(disciplines.router)
-app.include_router(competency_groups.router)
-app.include_router(competencies.router)
-app.include_router(indicators.router)
-app.include_router(direction_map_cors.router)
-app.include_router(map_cors.router)
-app.include_router(discipline_blocks.router)
-app.include_router(discipline_block_competencies.router)
-app.include_router(validations.router)
+app.include_router(educational_levels_router)
+app.include_router(educational_forms_router)
+app.include_router(directions_router)
+app.include_router(map_cors_router)
+app.include_router(direction_map_cors_router)
+app.include_router(departments_router)
+app.include_router(disciplines_router)
+app.include_router(activity_types_router)
+app.include_router(control_types_router)
+app.include_router(competency_groups_router)
+app.include_router(competencies_router)
+app.include_router(indicators_router)
+app.include_router(discipline_blocks_router)
+app.include_router(discipline_block_competencies_router)
+app.include_router(validations_router)
