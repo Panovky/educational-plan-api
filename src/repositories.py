72d3e5868289bf5b3e3b35990a23d23217f0064d
Ksenia Panova <rybinak95@gmail.com>
2025-05-15 17:11:58 +0300

    Move control types and competencies repository

--- src/repositories.py
+++ src/repositories.py
@@ -1,9 +1,7 @@
 from sqlalchemy.orm import Session
 from src.core.sqlalchemy_repository import SQLAlchemyRepository
 from src.indicators.model import Indicator
-from src.control_types.model import ControlType
 from src.competency_groups.model import CompetencyGroup
-from src.competencies.model import Competency
 from src.activity_types.model import ActivityType
 
 
@@ -12,19 +10,9 @@ class ActivityTypesRepository(SQLAlchemyRepository):
         super().__init__(session, ActivityType)
 
 
-class CompetenciesRepository(SQLAlchemyRepository):
-    def __init__(self, session: Session):
-        super().__init__(session, Competency)
-
-
 class CompetencyGroupsRepository(SQLAlchemyRepository):
     def __init__(self, session: Session):
         super().__init__(session, CompetencyGroup)
-
-
-class ControlTypesRepository(SQLAlchemyRepository):
-    def __init__(self, session: Session):
-        super().__init__(session, ControlType)
 
 
 class IndicatorsRepository(SQLAlchemyRepository):
