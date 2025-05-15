c21081cb73697a233d23eccac238bfdab345fed5
Ksenia Panova <rybinak95@gmail.com>
2025-05-15 16:16:41 +0300

    Move table discipline block activity types

--- src/models.py
+++ src/models.py
@@ -1,15 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, ForeignKey
+from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase):
     pass
-
-
-class DisciplineBlockActivityType(Base):
-    """Связи блоков дисциплин и видов учебных занятий."""
-    __tablename__ = 'discipline_block_activity_types'
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    discipline_block_id: Mapped[int] = mapped_column(Integer, ForeignKey('discipline_blocks.id'))
-    activity_type_id: Mapped[int] = mapped_column(Integer, ForeignKey('activity_types.id'))
